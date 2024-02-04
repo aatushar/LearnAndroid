b0dc4678982e937935a3929110cc7048b992636c
Md. Abdul Alim Tushar <132429007+aatushar@users.noreply.github.com>
2024-02-05 01:17:07 +0600

    Add
    Add

---
+++ LinkAnotherPage/app/src/main/java/com/example/linkanotherpage/SplashScreen.java
@@ -0,0 +1,43 @@
+package com.example.linkanotherpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Animation animation= AnimationUtils.loadAnimation(this, R.anim.zoom_in);
+        ImageView splashLogo=  findViewById(R.id.splashLogo);
+        splashLogo.setAnimation(animation);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent intent= new Intent(getApplicationContext(), Home.class);
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+    }
+}
